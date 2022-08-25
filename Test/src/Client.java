2592d7ea88a6c68d61d368ec5c446d2d82b855dc
haitrieule <59675645+haitrieule@users.noreply.github.com>
2022-08-25 14:18:18 +0700

    sfsf

--- Test/src/Client.java
+++ Test/src/Client.java
@@ -45,7 +45,7 @@ public class Client extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));  
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
